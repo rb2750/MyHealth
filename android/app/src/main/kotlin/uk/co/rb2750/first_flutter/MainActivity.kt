af3823f1e0fe8a00128d586be81aa0eb2d5a61e3
Rob Bladen <rb2750@live.co.uk>
2019-10-03 09:21:24 +0100

    Initial

---
+++ android/app/src/main/kotlin/uk/co/rb2750/first_flutter/MainActivity.kt
@@ -0,0 +1,13 @@
+package uk.co.rb2750.first_flutter
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
